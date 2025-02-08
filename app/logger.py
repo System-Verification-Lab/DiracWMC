a36f006cded62dc32e7e39f560a7adb92a34308b
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-02-08 12:16:25 +0100

    Added logger to new folder

---
+++ app/logger.py
@@ -0,0 +1,32 @@
+
+from typing import Any
+from datetime import datetime
+
+STAT_PADDING = 35
+
+class ConsoleColor:
+    BOLD = "\033[1m"
+    RED = "\033[91m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    CYAN = "\033[96m"
+    GREY = "\033[90m"
+    CLEAR = "\033[0m"
+
+def log_info(content: Any = ""):
+    """ Show an info message """
+    date = datetime.now().strftime("%H:%M:%S.%f")
+    print(f"{ConsoleColor.GREY}[{date}] {content}{ConsoleColor.CLEAR}")
+
+def log_warning(content: Any = ""):
+    """ Show a warning message """
+    date = datetime.now().strftime("%H:%M:%S.%f")
+    print(f"{ConsoleColor.YELLOW}[{date}] WARNING: {content}"
+    f"{ConsoleColor.CLEAR}")
+
+def log_stat(name: str = "", content: Any = "N/A"):
+    """ Show a statistic with the given name """
+    if name == "":
+        return
+    print(ConsoleColor.CYAN + (name + ":").ljust(STAT_PADDING) + " " +
+    ConsoleColor.CLEAR + str(content))

f76b344029ed12a66376a2731e28157b19bf9b51
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-02-08 15:29:11 +0100

    Added random graph experiment

---
+++ app/experiments/ising_random_graph.py
@@ -0,0 +1,24 @@
+
+from ..generators.ising import generate_random_graph
+from ._utils import do_ising_experiment
+from ..wcnf.solvers import SOLVERS, SolverType
+
+def run():
+    """ Run all random graph Ising experiments and print results to console """
+    results: dict[SolverType, list[tuple[int, float]]] = {}
+    for solver_type in SOLVERS:
+        results[solver_type] = []
+        size = 50
+        timed_out = False
+        while not timed_out:
+            model = generate_random_graph(size)
+            cur = do_ising_experiment(f"Random graph size={size}; solver="
+            f"{solver_type}", model, solver_type)
+            if cur < 0.0:
+                timed_out = True
+            else:
+                results[solver_type].append((size, cur))
+            size += 20
+    for solver_type, res in results.items():
+        print(f"Results for solver {solver_type}:")
+        print(" ".join(f"({r[0]}, {r[1]})" for r in res))
--- app/generators/ising.py
+++ app/generators/ising.py
@@ -26,3 +26,34 @@ def generate_square_lattice(size: int, weights: Literal["normal", "signed"] =
     for i in range(size * size):
         model[i] = random.normalvariate(0, 1)
     return model
+
+def generate_random_graph(size: int, avg_degree: float = 3.0, weights: Literal[
+"normal", "signed"] = "normal"):
+    """ Generate a random graph Ising model with a certain average degree. The
+        weights can be initialized using a normal distribution or a distribution
+        picking from -1 and +1 with equal probability """
+    model = IsingModel(size)
+    weight_func = {
+        "normal": (lambda: random.normalvariate(0, 1)),
+        "signed": (lambda: random.choice((-1, 1))),
+    }[weights]
+    edge_count = round(size * avg_degree / 2.0)
+    found: set[tuple[int, int]] = set()
+    # Interaction strengths
+    for _ in range(edge_count):
+        valid = False
+        edge = (0, 0)
+        while not valid:
+            edge = (random.randrange(size), random.randrange(size))
+            if edge[0] > edge[1]:
+                edge = (edge[1], edge[0])
+            if edge[0] == edge[1] or edge in found:
+                continue
+            found.add(edge)
+            valid = True
+        strength = weight_func()
+        model[edge] = strength
+    # External field strengths
+    for i in range(size):
+        model[i] = random.normalvariate(0, 1)
+    return model

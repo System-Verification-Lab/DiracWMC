83f80029879c900a4666f0445e79398b52e07fbf
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-02-27 14:22:01 +0100

    Added quantum ising generator

--- app/generators/quantum_ising.py
+++ app/generators/quantum_ising.py
@@ -4,8 +4,8 @@ import random
 from typing import Literal
 from ..models import QuantumIsingModel
 
-def generate_ring(size: int, weights: Literal["normal", "signed"] = "normal"
-) -> QuantumIsingModel:
+def generate_ring(size: int, weights: Literal["normal", "signed"] = "normal", *,
+line: bool = False) -> QuantumIsingModel:
     """ Generate a ring quantum Ising model with random weights. The weights
         can be initialized using a normal distribution or a distribution picking
         from -1 and +1 with equal probability """
@@ -14,7 +14,7 @@ def generate_ring(size: int, weights: Literal["normal", "signed"] = "normal"
         "normal": (lambda: random.normalvariate(0, 1)),
         "signed": (lambda: random.choice((-1, 1))),
     }[weights]
-    for i in range(size):
+    for i in range(size - 1 if line else size):
         strength = weight_func()
         model[i, (i + 1) % size] = strength
     model.external_field_x = random.normalvariate()

e31111be5c56a0dd026e185425e34b554f3ab07a
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-03-03 13:35:18 +0100

    Added ising square lattice experiment

--- app/experiments/ising_square_lattice.py
+++ app/experiments/ising_square_lattice.py
@@ -1,27 +1,42 @@
-""" Reproduce square lattice experiments from https://arxiv.org/pdf/2212.12812
-    """
 
-from ..generators.ising import generate_square_lattice
-from ._utils import do_ising_experiment
-from ..wcnf.solver import SOLVERS, SolverType
+from argparse import ArgumentParser
 
-def run():
-    """ Run all square lattice Ising experiments and print results to console
-        """
-    results: dict[SolverType, list[tuple[int, float]]] = {}
-    for solver_type in SOLVERS:
-        results[solver_type] = []
-        side_length = 2
-        timed_out = False
-        while not timed_out:
-            model = generate_square_lattice(side_length)
-            cur = do_ising_experiment(f"Square lattice l={side_length}; "
-            f"solver={solver_type}", model, solver_type)
-            if cur < 0.0:
-                timed_out = True
-            else:
-                results[solver_type].append((side_length, cur))
-            side_length += 1
-    for solver_type, res in results.items():
-        print(f"Results for solver {solver_type}:")
-        print(" ".join(f"({r[0]}, {r[1]})" for r in res))
+from ..wcnf.solver import SOLVERS, Solver
+from ..generator.ising import generate_square_lattice
+from ..converter import ising_to_wcnf
+
+BETA = 1.0
+
+parser = ArgumentParser(description="Reproduce square lattice experiments from "
+"https://arxiv.org/pdf/2212.12812. Outputs a space-separated list of tuples "
+"(size, runtime) (unless error calculation is enabled)")
+parser.add_argument("sizes", type=str, help="A comma-separated list of lattice "
+"sizes, or a string in the format start:stop:step where stop is not included")
+parser.add_argument("-s", "--solver", type=str, choices=SOLVERS, default="dpmc",
+help="The solver to use. Defaults to DPMC")
+parser.add_argument("-t", "--timeout", type=float, default=15.0, help="The "
+"timeout of the solver for each of the lattice sizes")
+parser.add_argument("-e", "--measure-error", action="store_true", default=False,
+help="Measure relative error instead of runtime and put this is output")
+args = parser.parse_args()
+
+if ":" in args.sizes:
+    sizes = list(range(*(int(x) for x in args.sizes.split(":"))))
+else:
+    sizes = [int(x) for x in args.sizes.split(",")]
+solver = Solver.from_solver_name(args.solver)
+results: list[tuple[int, float]] = []
+for size in sizes:
+    model = generate_square_lattice(size)
+    formula = ising_to_wcnf(model, BETA)
+    result = solver.run_solver(formula)
+    if not result.success:
+        raise RuntimeError("Solver timed out")
+    if args.measure_error:
+        true_weight = model.partition_function(BETA)
+        found_weight = result.total_weight
+        error = abs(found_weight / true_weight - 1.0)
+        results.append((size, error))
+    else:
+        results.append((size, result.runtime))
+print(" ".join(f"({r[0]}, {r[1]})" for r in results))

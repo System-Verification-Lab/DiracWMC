d0e3175119fca60bf6f7fe322112147120d5b043
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-04-24 16:05:20 +0200

    Added index basis element class

--- wcnf_matrix/wcnf_matrix/index.py
+++ wcnf_matrix/wcnf_matrix/index.py
@@ -8,3 +8,19 @@ class Index[Field]:
             field type """
         self.q = q
         self.field = field
+
+    def __getitem__(self, element: int) -> "IndexBasisElement[Field]":
+        """ Get one of the basis elements of this Hilbert space """
+        return IndexBasisElement(self, element)
+
+class IndexBasisElement[Field]:
+    """ A basis element in some Hilbert space, which can be |0>, ..., |q-1> """
+
+    def __init__(self, index: Index[Field], element: int):
+        """ Constructor given the index (Hilbert space of the basis element) and
+            the element, which can be 0, ..., q-1 """
+        self.index = index
+        self.element = element
+        if not 0 <= element < self.index.q:
+            raise ValueError(f"Element {element} is not in the standard basis "
+            f"of a {self.index.q}-state Hilbert space")

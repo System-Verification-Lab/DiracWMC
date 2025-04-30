8520f51c98dc954e8c57c3df52e6393f3e33c295
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-04-30 14:08:14 +0200

    Added proper implementation of value function

--- wcnf_matrix/wcnf_matrix/matrix/__init__.py
+++ wcnf_matrix/wcnf_matrix/matrix/__init__.py
@@ -1,12 +1,28 @@
 
+from .abstractmatrix import AbstractMatrix
 from .concretematrix import ConcreteMatrix
 from .labelmatrix import LabelMatrix
-Matrix = ConcreteMatrix
+from typing import overload
+
+type Matrix[Field] = ConcreteMatrix[Field]
 
 bra = Matrix.bra
 ket = Matrix.ket
 
+@overload
+def value[Field, MatrixType: AbstractMatrix[Field]](matrix: LabelMatrix[Field,
+MatrixType]) -> LabelMatrix[Field, ConcreteMatrix[Field]]:
+    """ Convert any LabelMatrix to a LabelMatrix using concrete values stored in
+        memory, instead of some other representation """
+    ...
+
+@overload
 def value[Field](matrix: Matrix[Field]) -> ConcreteMatrix[Field]:
+    """ Convert a matrix to a concrete matrix with actual values stored in
+        memory """
+    ...
+
+def value(matrix):
     """ Convert matrix to a concrete matrix with actual values stored in memory
         """
     return matrix.value()

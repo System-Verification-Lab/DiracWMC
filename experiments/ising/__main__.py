d54d8d395cfc608e86a8508c2379dd981a7bc895
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-06-05 12:50:39 +0200

    Added some square lattice experiments

--- experiments/ising/__main__.py
+++ experiments/ising/__main__.py
@@ -1,5 +1,78 @@
 
 from .converter import ising_to_wcnf
 from .ising_model import IsingModel
+from wcnf_matrix import ModelCounter, DPMC
+import random
 
-print(ising_to_wcnf(IsingModel(4)))
+SOLVERS: tuple[type[ModelCounter], ...] = (DPMC,)
+AVG_OVER_RUNS = 5
+random.seed(42)
+
+#############################################
+# Square lattice Ising model
+#############################################
+
+def generate_square_lattice(size: int) -> IsingModel:
+    """ Generate a square lattice Ising model with interaction and external
+        field strengths from the standard normal distribution. The side length
+        is given by size """
+    model = IsingModel(size * size)
+    # Interaction strengths
+    for i in range(size * size):
+        if i + size < size * size:
+            strength = random.normalvariate()
+            model[i, i + size] = strength
+        if i % size != size - 1:
+            strength = random.normalvariate()
+            model[i, i + 1] = strength
+    # External field strengths
+    for i in range(size * size):
+        model[i] = random.normalvariate(0, 1)
+    return model
+
+def experiment_square_lattice():
+    for solver in SOLVERS:
+        model_counter = solver()
+        print(f"\nSquare lattice {solver.__name__}:")
+        for size in range(2, 23):
+            models = [generate_square_lattice(size) for _ in
+            range(AVG_OVER_RUNS)]
+            problems = [ising_to_wcnf(model) for model in models]
+            failed = False
+            runtime = 0.0
+            for result in model_counter.batch_model_count(*problems):
+                if not result.success:
+                    failed = True
+                    break
+                runtime += result.runtime
+            if failed:
+                break
+            print(f"({size}, {runtime / AVG_OVER_RUNS})", end=" ", flush=True)
+        print()
+
+def experiment_square_lattice_accuracy():
+    for solver in SOLVERS:
+        model_counter = solver()
+        print(f"\nSquare lattice accuracy {solver.__name__}:")
+        for size in range(2, 5):
+            models = [generate_square_lattice(size) for _ in
+            range(AVG_OVER_RUNS)]
+            problems = [ising_to_wcnf(model) for model in models]
+            failed = False
+            error = 0.0
+            for result, model in zip(model_counter.batch_model_count(*problems),
+            models):
+                if not result.success:
+                    failed = True
+                    break
+                error += abs(result.model_count / model.partition_function() -
+                1)
+            if failed:
+                break
+            print(f"({size}, {error / AVG_OVER_RUNS})", end=" ", flush=True)
+        print()
+
+#############################################
+
+experiment_square_lattice()
+experiment_square_lattice_accuracy()
--- experiments/ising/converter.py
+++ experiments/ising/converter.py
@@ -1,6 +1,7 @@
 
 from .ising_model import IsingModel
 from wcnf_matrix import CNF, WeightFunction, BoolVar, WCNFMatrix
+import numpy as np
 
 def ising_to_wcnf(model: IsingModel, beta: float = 1.0) -> tuple[CNF,
 WeightFunction]:
@@ -8,7 +9,30 @@ WeightFunction]:
         such that the weighted model count of the formula w.r.t. the weight
         function is equal to the partition function of the Ising model at
         inverse temperature beta """
-    ...
+    # Indices 0,..,n-1 correspond with nodes, n,..,m+n-1 with interactions
+    n = len(model)
+    interactions = list(model.interactions())
+    bool_vars = [BoolVar() for _ in range(n + len(interactions))]
+    cnf = CNF()
+    weight_func = WeightFunction(bool_vars)
+    # For every interaction, add clause ((i <=> j) <=> ij)
+    for index, (i, j, _) in enumerate(interactions, n):
+        cnf.add_clause(
+            [bool_vars[i], -bool_vars[j], -bool_vars[index]],
+            [bool_vars[i], bool_vars[j], bool_vars[index]],
+            [-bool_vars[i], bool_vars[j], -bool_vars[index]],
+            [-bool_vars[i], -bool_vars[j], bool_vars[index]],
+        )
+    # Weight of variables is e^(beta*external[i]*(2*tau[i]-1)), where tau[i] is
+    # 0 or 1 and indices truth value of variable i
+    for index, external_field in enumerate(model.external_field):
+        weight_func[bool_vars[index], True] = np.exp(beta * external_field)
+        weight_func[bool_vars[index], False] = np.exp(-beta * external_field)
+    # Weight of connections is e^(beta*strength*(2*tau[ij]-1))
+    for index, (_, _, strength) in enumerate(interactions, n):
+        weight_func[bool_vars[index], True] = np.exp(beta * strength)
+        weight_func[bool_vars[index], False] = np.exp(-beta * strength)
+    return cnf, weight_func
 
 def ising_to_wcnf_matrix(model: IsingModel, beta: float = 1.0) -> WCNFMatrix:
     """ Convert the given Ising model to a WCNFMatrix, the trace of which is

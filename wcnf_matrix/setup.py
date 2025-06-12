d70875623a6366ac937d047a44a27451c45e748f
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-06-12 11:28:44 +0200

    Added RUNNER_VERSION to setup.py

--- wcnf_matrix/setup.py
+++ wcnf_matrix/setup.py
@@ -3,17 +3,20 @@ import docker
 import os
 import setuptools
 
+RUNNER_VERSION = "1.5"
+
 def install_dpmc():
     """ Install the DPMC solver using Docker """
     print("Installing DPMC...")
     docker_build_path = os.path.join(os.path.dirname(__file__), "wcnf_matrix",
     "cnf", "model_counter", "solver", "DPMC")
     client = docker.from_env()
+    tag = f"dpmc{RUNNER_VERSION}"
     try:
-        client.images.get("dpmc:latest")
+        client.images.get(f"{tag}:latest")
         print("DPMC already installed!")
     except:
-        client.images.build(path=docker_build_path, tag="dpmc")
+        client.images.build(path=docker_build_path, tag=tag)
 
 def install_cachet():
     """ Install the Cachet solver using Docker """
@@ -21,11 +24,12 @@ def install_cachet():
     docker_build_path = os.path.join(os.path.dirname(__file__), "wcnf_matrix",
     "cnf", "model_counter", "solver", "cachet")
     client = docker.from_env()
+    tag = f"cachet{RUNNER_VERSION}"
     try:
-        client.images.get("cachet:latest")
+        client.images.get(f"{tag}:latest")
         print("Cachet already installed!")
     except:
-        client.images.build(path=docker_build_path, tag="cachet")
+        client.images.build(path=docker_build_path, tag=tag)
 
 def install_tensororder():
     """ Install the TensorOrder solver using Docker """
@@ -33,11 +37,12 @@ def install_tensororder():
     docker_build_path = os.path.join(os.path.dirname(__file__), "wcnf_matrix",
     "cnf", "model_counter", "solver", "TensorOrder")
     client = docker.from_env()
+    tag = f"tensororder{RUNNER_VERSION}"
     try:
-        client.images.get("tensororder:latest")
+        client.images.get(f"{tag}:latest")
         print("TensorOrder already installed!")
     except:
-        client.images.build(path=docker_build_path, tag="tensororder")
+        client.images.build(path=docker_build_path, tag=tag)
 
 install_dpmc()
 install_cachet()

fe2a77908ae982a709e638e6cecb616ba95e4082
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-06-12 13:38:04 +0200

    Finished Potts model experiments

--- experiments/potts/__main__.py
+++ experiments/potts/__main__.py
@@ -1,11 +1,13 @@
 
 from .converter import standard_potts_to_wcnf_matrix
 from .standard_potts_model import StandardPottsModel
-from wcnf_matrix import ModelCounter, DPMC, Cachet, TensorOrder
+from wcnf_matrix import ModelCounter, DPMC, Cachet, TensorOrder, set_var_rep_type, OrderVarRep
 import random
 
 SOLVERS: tuple[type[ModelCounter], ...] = (DPMC,)
 AVG_OVER_RUNS = 5
+
+# set_var_rep_type(OrderVarRep)
 
 #############################################
 # Random regular graph standard Potts model
@@ -32,7 +34,7 @@ def experiment_random_regular_graph(q: int):
         model_counter = solver()
         print(f"\nRandom graph {solver.__name__} (q={q}):")
         for size in range(8, 70, 4):
-            models = [generate_random_graph(size, q, 3) for _ in
+            models = [generate_random_graph(size, q, 4) for _ in
             range(AVG_OVER_RUNS)]
             problems = [standard_potts_to_wcnf_matrix(model).trace_formula() for
             model in models]
@@ -49,7 +51,34 @@ def experiment_random_regular_graph(q: int):
             print(f"({size}, {runtime / AVG_OVER_RUNS})", end=" ", flush=True)
         print()
 
+def experiment_random_regular_graph_accuracy(q: int):
+    random.seed(42)
+    for solver in SOLVERS:
+        model_counter = solver()
+        print(f"\nRandom graph accuracy {solver.__name__} (q={q}):")
+        for size in range(2, 10):
+            models = [generate_random_graph(size, q, 4) for _ in
+            range(AVG_OVER_RUNS)]
+            problems = [standard_potts_to_wcnf_matrix(model).trace_formula() for
+            model in models]
+            failed = False
+            error = 0.0
+            for result, model in zip(model_counter.batch_model_count(*problems),
+            models):
+                if not result.success:
+                    failed = True
+                    break
+                error += abs(result.model_count / model.partition_function() -
+                1)
+            if failed:
+                print("FAILURE")
+                break
+            print(f"({size}, {error / AVG_OVER_RUNS})", end=" ", flush=True)
+        print()
+
 #############################################
 
-for q in range(2, 6):
+for q in range(2, 4):
+    experiment_random_regular_graph_accuracy(q)
+for q in range(2, 9):
     experiment_random_regular_graph(q)
